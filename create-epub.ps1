e509b3ae73a443b169d511b19b7c7879082174a0
David Porter <david.porter@fastmail.com>
2022-09-08 08:45:35 +1000

    add epub script

---
+++ create-epub.ps1
@@ -0,0 +1,20 @@
+#delete book if exsiting
+remove-item annotated-hlc.epub -ea ig
+
+#delete dist folder
+remove-item dist -recurse -force
+
+# build the project
+npm run build
+
+# reset the name of mimetype so that it will be first item in the archive
+rename-item -path dist\mimetype -newname !mimetype
+
+#create the archive
+C:\"Program Files"\7-Zip\7z a -tzip njb.epub .\dist\*
+
+# rename !mimetype  
+C:\"Program Files"\7-Zip\7z rn njb.epub !mimetype mimetype
+
+
+
